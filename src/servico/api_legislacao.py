02733703c86e4cdc8bfa8ac8c0057546fa6f7889
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-01-22 20:45:29 -0300

    método para inserção do banco

---
+++ src/servico/api_legislacao.py
@@ -0,0 +1,30 @@
+import requests
+# from airflow.models import Variable
+from datetime import datetime, timedelta
+from typing import List
+# https://dadosabertos.almg.gov.br
+
+
+class APILegislacao:
+    def __init__(self):
+        # self.__URL_BASE = Variable.get('api_dados_abertos_mg')
+        self.__URL_BASE = 'https://dadosabertos.almg.gov.br'
+        self.__data_final = datetime.now().strftime('%Y%m%d')
+        self.__intervalo_dias = 60
+        self.__data_inicial = (
+            datetime.now() - timedelta(days=self.__intervalo_dias)).strftime('%Y%m%d')
+
+    def obter_proposicoes(self) -> List:
+        """Método para obter as proposicoes
+
+        Returns:
+            List: _description_
+        """
+        url = f'{self.__URL_BASE}/ws/proposicoes/pesquisa/direcionada?tp=1000&formato=json&ord=3&p=1&ini={self.__data_inicial}&fim={self.__data_inicial}'
+        req = requests.get(url=url)
+        req = req.json()
+        return req['resultado']['listaItem']
+
+
+a = APILegislacao()
+print(type(a.obter_proposicoes()))
---
+++ src/servico/opercacoes_banco.py
@@ -0,0 +1,15 @@
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+
+
+class OperacaoBanco:
+    def __init__(self):
+        self.__id_conexao_mssql = 'sql_server_airflow'
+        self.__mssql_hook = MsSqlHook(mssql_conn_id=self.__id_conexao_mssql)
+
+    def realizar_operacao_banco(self, consulta: str):
+        """Método para realizar operações no banco
+
+        Args:
+            consulta (str): _description_
+        """
+        self.__mssql_hook.run(sql=consulta)
