ae0c83eca954c1e0a5a20a6ee8ce2cad64121630
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-01-26 00:04:05 -0300

    modularizando função

--- dags/src/etl.py
+++ dags/src/etl.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Dict
 from src.servico.i_opecacoes_banco import IOperacoesBanco
 from src.servico.i_servico_api import IServicoAPI
 from pymssql.exceptions import IntegrityError, DatabaseError
@@ -16,22 +17,7 @@ class ETL:
         for proposicao in self.__api_legislacao.obter_proposicoes():
             try:
 
-                dados_info = {
-                    'TIPO_LOG': 'INFO',
-                    'MENSAGEM_LOG': 'OBTENÇÃO RESULTADO',
-                    'JSON_XML': json.dumps(proposicao)
-                }
-
-                sql_info = f"""
-                    INSERT INTO log_dag (TIPO_LOG, MENSAGEM_LOG, JSON_XML)
-                    VALUES (%(TIPO_LOG)s, %(MENSAGEM_LOG)s, %(JSON_XML)s )
-                """
-                self.__operacoes_banco.realizar_operacao_banco(
-                    consulta=sql_info,
-                    parametros=dados_info,
-
-
-                )
+                self.__registrar_log()
                 try:
                     assunto = re.sub(r'[^\w\s.,;]', '',
                                      proposicao['assunto']).strip().replace('\n', '')
@@ -88,74 +74,59 @@ class ETL:
                     consulta=sql_insersao, parametros=dado)
             except KeyError as msg:
 
-                proposicao = json.dumps(proposicao)
-                dados_erro = {
-                    'NUMERO': numero,
-                    'JSON_XML': proposicao,
-                    'MENSAGEM_ERRO': f'Não encontrou a chave KeyError: {msg}',
-                    'DATA_ATUALIZACAO': data_registro
-                }
+                mensagem_erro = f'Não encontrou a chave KeyError: {msg}'
+                self.__registrar_erro(
+                    json_xml=proposicao, numero=numero, data_registro=data_registro, mensagem_erro=mensagem_erro)
 
-                sql_erro = f"""
-                    INSERT INTO dag_error (NUMERO, JSON_XML, MENSAGEM_ERRO, DATA_ATUALIZACAO)
-                    VALUES (%(NUMERO)s, %(JSON_XML)s, %(MENSAGEM_ERRO)s, %(DATA_ATUALIZACAO)s)
-                """
-                self.__operacoes_banco.realizar_operacao_banco(
-                    consulta=sql_erro,
-                    parametros=dados_erro
-                )
             except IntegrityError as msg:
 
-                proposicao = json.dumps(proposicao)
-                dados_erro = {
-                    'NUMERO': numero,
-                    'JSON_XML': proposicao,
-                    'MENSAGEM_ERRO': f'Já existe a chave, {numero}',
-                    'DATA_ATUALIZACAO': data_registro
-                }
-
-                sql_erro = f"""
-                    INSERT INTO dag_error (NUMERO, JSON_XML, MENSAGEM_ERRO, DATA_ATUALIZACAO)
-                    VALUES (%(NUMERO)s, %(JSON_XML)s, %(MENSAGEM_ERRO)s, %(DATA_ATUALIZACAO)s)
-                """
-                self.__operacoes_banco.realizar_operacao_banco(
-                    consulta=sql_erro,
-                    parametros=dados_erro
-                )
+                mensagem_erro = f'Já existe a chave, {numero}'
+                self.__registrar_erro(
+                    json_xml=proposicao, numero=numero, data_registro=data_registro, mensagem_erro=mensagem_erro)
 
             except DatabaseError as msg:
 
-                proposicao = json.dumps(proposicao)
-                dados_erro = {
-                    'NUMERO': numero,
-                    'JSON_XML': proposicao,
-                    'MENSAGEM_ERRO': f'Dados invalidos em {str(msg.args[1]).split(', ')[1]}',
-                    'DATA_ATUALIZACAO': data_registro
-                }
-
-                sql_erro = f"""
-                    INSERT INTO dag_error (NUMERO, JSON_XML, MENSAGEM_ERRO, DATA_ATUALIZACAO)
-                    VALUES (%(NUMERO)s, %(JSON_XML)s, %(MENSAGEM_ERRO)s, %(DATA_ATUALIZACAO)s)
-                """
-                self.__operacoes_banco.realizar_operacao_banco(
-                    consulta=sql_erro,
-                    parametros=dados_erro
-                )
+                mensagem_erro = f'Dados invalidos em {str(msg.args[1]).split(', ')[1]}'
+                self.__registrar_erro(
+                    json_xml=proposicao, numero=numero, data_registro=data_registro, mensagem_erro=mensagem_erro)
 
             except Exception as msg:
-                proposicao = json.dumps(proposicao)
-                dados_erro = {
-                    'NUMERO': numero,
-                    'JSON_XML': proposicao,
-                    'MENSAGEM_ERRO': f'Erro Fatal',
-                    'DATA_ATUALIZACAO': data_registro
-                }
+                mensagem_erro = 'Erro fatal'
+                self.__registrar_erro(
+                    json_xml=proposicao, numero=numero, data_registro=data_registro, mensagem_erro=mensagem_erro)
 
-                sql_erro = f"""
+    def __registrar_erro(self, json_xml: Dict, numero: str, data_registro, mensagem_erro):
+        json_xml = json.dumps(json_xml)
+        dados_erro = {
+            'NUMERO': numero,
+            'JSON_XML': json_xml,
+            'MENSAGEM_ERRO': mensagem_erro,
+            'DATA_ATUALIZACAO': data_registro
+        }
+
+        sql_erro = f"""
                     INSERT INTO dag_error (NUMERO, JSON_XML, MENSAGEM_ERRO, DATA_ATUALIZACAO)
                     VALUES (%(NUMERO)s, %(JSON_XML)s, %(MENSAGEM_ERRO)s, %(DATA_ATUALIZACAO)s)
                 """
-                self.__operacoes_banco.realizar_operacao_banco(
-                    consulta=sql_erro,
-                    parametros=dados_erro
-                )
+        self.__operacoes_banco.realizar_operacao_banco(
+            consulta=sql_erro,
+            parametros=dados_erro
+        )
+
+    def __registrar_log(self, json_xml: Dict):
+        dados_info = {
+            'TIPO_LOG': 'INFO',
+            'MENSAGEM_LOG': 'OBTENÇÃO RESULTADO',
+            'JSON_XML': json.dumps(json_xml)
+        }
+
+        sql_info = f"""
+                    INSERT INTO log_dag (TIPO_LOG, MENSAGEM_LOG, JSON_XML)
+                    VALUES (%(TIPO_LOG)s, %(MENSAGEM_LOG)s, %(JSON_XML)s )
+                """
+        self.__operacoes_banco.realizar_operacao_banco(
+            consulta=sql_info,
+            parametros=dados_info,
+
+
+        )
